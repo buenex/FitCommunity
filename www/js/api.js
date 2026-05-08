eed47e866a28b232585977efb1f9153edd272636
buenex <43445161+buenex@users.noreply.github.com>
2026-05-08 00:27:59 -0300

    Update api.js

--- www/js/api.js
+++ www/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3000/api'
+const API_BASE = 'https://buenex-my-apis.duckdns.org/fit-community/api'
 
 function getToken() {
   return localStorage.getItem('community_fit_token')
